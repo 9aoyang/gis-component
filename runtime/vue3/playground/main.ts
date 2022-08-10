67363ce5901febe8fa88161c851ba20c2fec4027
roymondchen <roymondchen@tencent.com>
2022-08-10 18:19:47 +0800

    feat(runtime): 添加openlayers插件

--- runtime/vue3/playground/main.ts
+++ runtime/vue3/playground/main.ts
@@ -2,12 +2,9 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 
-import OpenLayersMap from 'vue3-openlayers'
-import 'vue3-openlayers/dist/vue3-openlayers.css'
 
 Promise.all([import('../.tmagic/comp-entry'), import('../.tmagic/plugin-entry')]).then(([components, plugins]) => {
   const magicApp = createApp(App);
-  magicApp.use(OpenLayersMap)
 
   Object.values(components.default).forEach((component: any) => {
     magicApp.component(component.name, component);
---
+++ runtime/vue3/plugins/openlayers/index.ts
@@ -0,0 +1,7 @@
+import OpenLayersMap from 'vue3-openlayers';
+
+export default {
+  install(app) {
+    app.use(OpenLayersMap);
+  },
+};
